09701168b7af41780c0181444b3941e13ec806ed
YojanserBrito <55768554+YojanserBrito@users.noreply.github.com>
2019-12-09 21:33:33 -0400

    Final

---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+       
+
+        [HttpPost]
+        public ActionResult Verificar(Account cuenta)
+        {
+            try
+            {
+                using (webFinalEntities db = new webFinalEntities())
+                {
+                    var consulta_user = db.users.Where(x => x.nombre_usuario == cuenta.User).FirstOrDefault();
+                    var consulta_pass = db.users.Where(x => x.password_usuario == cuenta.Password).FirstOrDefault();
+
+                    if (!(consulta_user == null && consulta_pass == null))
+                    {
+
+                        return RedirectToAction("Index", "Socios");
+
+                    }
+                    else
+                    {
+                        return View("Error");
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("Error", ex.Message);
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplication1/Transversal/Dto/SocioDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication1.Controllers;
+
+namespace WebApplication1.Transversal.Dto
+{
+    public class SocioDto
+    {
+        public byte[] Foto { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public string cedula { get; set; }
+        public string Direccion { get; set; }
+        public string Sexo { get; set; }
+        public string Telefono { get; set; }
+        public int Edad { get; set; }
+        [Display(Name = "Fecha de Nacimiento")]
+        [DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yy}", ApplyFormatInEditMode = true)] public System.DateTime Fecha_Nacimiento { get; set; }
+        public string Lugar_De_Trabajo { get; set; }
+        public string Direccion_Trabajo { get; set; }
+        public string Telefono_Oficina { get; set; }
+        public string TipoMembresiaId { get; set; }
+        //public List<Afiliado> ListAfiliados { get; set; }
+
+
+    }
+}
